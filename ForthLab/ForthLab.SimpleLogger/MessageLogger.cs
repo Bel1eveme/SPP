b1701f8ccf66c8d6caa5ef681cea6b1987cbfb2e
Alexandr Ravodin <alex180103@gmail.com>
2024-04-23 13:10:16 +0300

    fix message logger

--- ForthLab/ForthLab.SimpleLogger/MessageLogger.cs
+++ ForthLab/ForthLab.SimpleLogger/MessageLogger.cs
@@ -3,48 +3,93 @@
 namespace ForthLab.MessageLogger;
 
 public class MessageLogger
-{
-    private readonly LoggerConfiguration _loggerConfiguration;
-
-    private readonly ConcurrentQueue<string> _messages;
-
-    private int _messageCount;
-    
-    public MessageLogger(LoggerConfiguration loggerConfiguration)
     {
-        _loggerConfiguration = loggerConfiguration;
-
-        _messages = [];
-        _messageCount = 0;
-    }
-    
-    public async Task AddMessage(string message)
-    {
-        _messages.Enqueue(message);
-        var currentMessageCount = Interlocked.Increment(ref _messageCount);
+        private readonly LoggerConfiguration _config;
         
-        if (currentMessageCount == _loggerConfiguration.ResetMessageCount)
-        {
-            await Flush();
-            //await RestartScheduler();
-        }
-    }
+        private readonly ConcurrentQueue<string> _messages;
+        private readonly SemaphoreSlim _flushSemaphore;
+        
+        private CancellationTokenSource _cts;
+        private Task _timerTask;
 
-    public async Task Flush()
-    {
-        var messagesToDeleteCount = _loggerConfiguration.ResetMessageCount;
-        List<string> messagesToDelete = [];
-
-        for (int i = 0; i < messagesToDeleteCount; i++)
+        public MessageLogger(LoggerConfiguration config)
         {
-            if (!_messages.TryDequeue(out var message))
-            {
-                throw new Exception();
-            }
+            _config = config;
             
-            messagesToDelete.Add(message);
+            _messages = new ConcurrentQueue<string>();
+            _flushSemaphore = new SemaphoreSlim(1, 1);
+            _cts = new CancellationTokenSource();
+            
+            _timerTask = StartScheduledFlush(_cts.Token);
         }
-        
-        await File.WriteAllLinesAsync(_loggerConfiguration.FilePath, messagesToDelete);
+
+        public void Add(string message)
+        {
+            _messages.Enqueue($"{DateTime.UtcNow}: {message}");
+            
+            CheckFlushCondition();
+        }
+
+        private void CheckFlushCondition()
+        {
+            if (_messages.Count >= _config.ResetMessageCount)
+            {
+                _cts.Cancel();
+            }
+        }
+
+        private void ResetTimer()
+        {
+            _cts.Dispose();
+            _cts = new CancellationTokenSource();
+            _timerTask = StartScheduledFlush(_cts.Token);
+        }
+
+        private async Task StartScheduledFlush(CancellationToken cancellationToken)
+        {
+            try
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    await Task.Delay(_config.ResetTimeout, cancellationToken);
+                    await FlushAsync();
+                }
+            }
+            catch (TaskCanceledException)
+            {
+                await FlushAsync();
+                ResetTimer();
+            }
+        }
+
+        private async Task FlushAsync()
+        {
+            await _flushSemaphore.WaitAsync();
+            try
+            {
+                var messagesToWrite = new List<string>();
+                while (_messages.TryDequeue(out var msg))
+                {
+                    messagesToWrite.Add(msg);
+                }
+
+                if (messagesToWrite.Count > 0)
+                {
+                    await File.AppendAllLinesAsync(_config.FilePath, messagesToWrite);
+                }
+            }
+            finally
+            {
+                _flushSemaphore.Release();
+            }
+        }
+
+        public async Task StopAsync()
+        {
+            await _cts.CancelAsync();
+            await _timerTask;
+            await FlushAsync(); 
+            _cts.Dispose();
+            _flushSemaphore.Dispose();
+        }
     }
-}
